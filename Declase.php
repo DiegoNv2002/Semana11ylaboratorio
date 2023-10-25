f173c8044b4cb4421ce3f2dea23cbdc93f1937c7
DiegoNv2002 <143531846+DiegoNv2002@users.noreply.github.com>
2023-10-25 16:30:33 -0500

    Declase.php

---
+++ Declase.php
@@ -0,0 +1,18 @@
+<?php
+    $prod = $_POST["producto"];
+    $cant = $_POST["cantidad"];
+    $prec = $_POST["precio"];
+    $desc=0;
+    if($cant>12)
+        $desc=0.15*$prec;
+        $stot=$cant*$prec;
+        $tot=$stot-($desc*$cant);
+?>
+<h3>
+    Producto: <?=$prod?>
+    <br>Cantidad :<?=$cant?>
+    <br>Precio :<?=$prec?>
+    <br>Descuento :<?=$desc?>
+    <br>Subtotal :<?=$stot?>
+    <br>Total :<?=$tot?>
+</h3>
